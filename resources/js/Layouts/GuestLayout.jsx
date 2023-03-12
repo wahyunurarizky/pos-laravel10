4992f11bb17f8dd8f362f8ad88b00b9e2a09929a
wahyunurarizky <arizky.wna@gmail.com>
2023-03-12 07:48:43 +0700

    rapiin login

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,12 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-96 h-96 text-gray-500" />
+                    <ApplicationLogo className="w-72 h-72 text-gray-500" />
                 </Link>
             </div>
 
